03ffc6db4035d110e05f7aea29cf2c763cad1079
Willem <willh445447@hotmail.com>
2019-09-03 20:26:16 -0500

    Add previously ignored CSV files
    develop

---
+++ PasswordVault/CSV/Reader/ICSVReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordVault
+{
+    interface ICSVReader
+    {
+        void Initialize(string fileName);
+        void Close();
+        string ReadLine();
+    }
+}
