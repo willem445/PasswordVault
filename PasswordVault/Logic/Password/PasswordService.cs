307dc54d4059cb52019470d87a3a2b8bc3c38c69
Willem <willh445447@hotmail.com>
2019-09-21 10:00:57 -0500

    Add fix for edit duplicate password issue
    fixes #39
    
    IS24_CreateCustomContextMenu

--- PasswordVault/Logic/Password/PasswordService.cs
+++ PasswordVault/Logic/Password/PasswordService.cs
@@ -298,7 +298,8 @@ namespace PasswordVault
                                                              where pass.Application == modifiedPassword.Application
                                                              select pass).ToList<Password>();
 
-                    if (modifiedPasswordResult.Count <= 0)
+                    if ((modifiedPasswordResult.Count <= 0) || // verify that another password doesn't have the same application name
+                        (modifiedPassword.Application == originalPassword.Application)) // if the application name of the original and modified match, continue as this is not actually a duplicate
                     {
                         Password modifiedEncryptedPassword = ConvertPlaintextPasswordToEncryptedPassword(modifiedPassword);
 
