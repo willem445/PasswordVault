087483a51bd974f563c9d6c9491cf3f7b68d21aa
Willem <willh445447@hotmail.com>
2019-10-18 06:44:39 -0500

    Convert models and data library to dotnet standard
    Add simple get method that returns list of Passwords by UUID.
    
    IS63_CreateWebApiProject

---
+++ PasswordVault.WebApi/Controllers/PasswordController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PasswordVault.Data.Standard;
+using PasswordVault.Models.Standard;
+
+namespace PasswordVault.WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PasswordController : ControllerBase
+    {
+        // GET: api/Password/5
+        [HttpGet("{uuid}", Name = "Get")]
+        public IEnumerable<Password> Get(string uuid)
+        {
+            IDatabase _dbcontext = new SQLiteDatabase();
+
+            List<Password> passwords = new List<Password>();
+
+            foreach (var item in _dbcontext.GetUserPasswordsByGUID(uuid))
+            {
+                Password password = new Password(
+                    item.UniqueID,
+                    item.Application,
+                    item.Username,
+                    item.Email,
+                    item.Description,
+                    item.Website,
+                    item.Passphrase 
+                );
+
+                passwords.Add(password);
+            }
+
+            return passwords;
+        }
+
+        // POST: api/Password
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Password/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
