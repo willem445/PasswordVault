08bbef8bbc2c23fcf7c4cf88b8c68bdc86a8d861
willem445 <willh445447@hotmail.com>
2019-11-29 20:52:47 -0600

    Add skeleton test for password count
    IS90_AddPasswordCount

---
+++ PasswordVault.ServicesTests/PasswordService/Password/PasswordCountTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordVault.Data;
+using PasswordVault.Services;
+using PasswordVault.Models;
+using System.Diagnostics;
+using PasswordVault.Desktop.Winforms;
+
+namespace PasswordVault.ServicesTests
+{
+    /// <summary>
+    /// Summary description for AddPasswordTest
+    /// </summary>
+    [TestClass]
+    public class PasswordCountTests
+    {
+        IDatabase db;
+        IDesktopServiceWrapper passwordService;
+        AddUserResult createUserResult;
+        AuthenticateResult loginResult;
+        LogOutResult logoutResult;
+        AddModifyPasswordResult addPasswordResult;
+        User user;
+
+        public PasswordCountTests()
+        {
+            
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+
+        // Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void MyTestInitialize() 
+        {
+            db = DatabaseFactory.GetDatabase(Database.InMemory);
+            passwordService = DesktopPasswordServiceBuilder.BuildDesktopServiceWrapper(db);
+
+            user = new User("testAccount", "testPassword1@aaaaaaaaa", "testFirstName", "testLastName", "222-111-1111", "test@test.com");
+            createUserResult = passwordService.CreateNewUser(user);
+            Assert.AreEqual(AddUserResult.Successful, createUserResult);
+            Assert.AreEqual(1, ((InMemoryDatabase)db).LocalUserDbAccess.Count);
+
+            loginResult = passwordService.Login("testAccount", "testPassword1@aaaaaaaaa");
+            Assert.AreEqual(AuthenticateResult.Successful, loginResult);
+        }
+
+        // Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            ((InMemoryDatabase)db).LocalPasswordDbAccess.Clear();
+            ((InMemoryDatabase)db).LocalUserDbAccess.Clear();
+
+            logoutResult = passwordService.Logout();
+            Assert.AreEqual(LogOutResult.Success, logoutResult);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 
+        /// 
+        /// Pass Criteria:
+        /// - 
+        /// </summary>
+        [TestMethod]
+        public void PasswordCountTest()
+        {
+            Password password = new Password("App1", "username", "email@email.com", "descriptions", "https://www.website.com", "passphrase");
+            addPasswordResult = passwordService.AddPassword(password);
+            Assert.AreEqual(AddModifyPasswordResult.Success, addPasswordResult);
+            Assert.AreEqual(1, ((InMemoryDatabase)db).LocalPasswordDbAccess.Count);
+
+
+            Assert.Fail();
+        }
+    }
+}
