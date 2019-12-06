e5d93aeb031335afe1b0f55965f23e9584f0a544
willem445 <willh445447@hotmail.com>
2019-12-05 20:08:05 -0600

    Add EPPlus package for writing to excel
    IS13_ExportPasswordsToExcel

--- PasswordVault.Services/ExportPasswordsService/ExcelExport.cs
+++ PasswordVault.Services/ExportPasswordsService/ExcelExport.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using OfficeOpenXml;
 
 namespace PasswordVault.Services
 {
@@ -9,6 +11,13 @@ namespace PasswordVault.Services
         public ExportResult Export(string exportPath)
         {
             ExportResult result = ExportResult.Fail;
+
+            using (var p = new ExcelPackage())
+            {
+                var ws = p.Workbook.Worksheets.Add("Passwords");
+                ws.Cells["A1"].Value = "This is cell A1";
+                p.SaveAs(new FileInfo(@"C:\Users\WillH\Desktop\PasswordBackup.xlsx"));
+            }
 
             return result;
         }
